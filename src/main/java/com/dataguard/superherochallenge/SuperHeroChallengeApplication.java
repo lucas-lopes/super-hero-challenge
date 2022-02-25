9a81b08d0a78fac2e3f074265bfad2f662093760
Lucas Barbosa <lucas.lopesifrn@gmail.com>
2022-02-25 15:39:26 -0300

    Initial commit

---
+++ src/main/java/com/dataguard/superherochallenge/SuperHeroChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.dataguard.superherochallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperHeroChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperHeroChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dataguard/superherochallenge/SuperHeroChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataguard.superherochallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperHeroChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
